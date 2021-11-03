5e1b9836632237220c17e6eaf63fc287580bb0c0
Misha Sokil <rasiapypkin2014@gmail.com>
2021-11-03 20:38:11 +0200

    Update Product.java

--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -1,6 +1,8 @@
 package com.company;
 
-public class Product {
+import java.io.Serializable;
+
+public class Product implements Serializable {
     private final int amount;
     private final int price;
     private final int year;
